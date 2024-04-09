ade3df1c98886f5352b55c88fcaac63d4af78f75
koreanpro22 <dhskim22@gmail.com>
2024-04-09 19:49:07 -0400

    minor setup changes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json())
 
-app.listen(5000, () => {
-    console.log("Server has started on port 5000");
+app.listen(8000, () => {
+    console.log("Server has started on port 8000");
 });
