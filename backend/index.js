46c1879feb4613a2f96337391185f813820d0540
koreanpro22 <dhskim22@gmail.com>
2024-04-09 19:05:30 -0400

    changed gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+// const cors = require("cors");
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json())
 
 app.listen(5000, () => {
