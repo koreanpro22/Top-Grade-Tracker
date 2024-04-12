d532d2d5a2bf005174cf559decbffa3387b47412
koreanpro22 <dhskim22@gmail.com>
2024-04-12 18:47:27 -0400

    working on seeders

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,16 +1,14 @@
 const express = require("express");
-import { PrismaClient } from '@prisma/client'
+import { db } from '../src/utils/db.server';
 
-const prisma = new PrismaClient()
 const router = express.Router();
 
 router.post('/', async (req: object, res: object) => {
     const { name, email, password, profilePicture, role, phone } = req.body;
 
-  const user = await prisma.post.create({
+  const user = await db.user.create({
     data: { name, email, password, profilePicture, role, phone }
   })
 
-  res.json(user)
 
 })
