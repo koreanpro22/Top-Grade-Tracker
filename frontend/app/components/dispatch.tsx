9d8c04c7f1c002f90837966aa6456ca353b7969f
Bao <bao4ltyfe@gmail.com>
2024-04-24 13:38:49 -0700

    getting caught up

--- frontend/app/components/dispatch.tsx
+++ frontend/app/components/dispatch.tsx
@@ -9,6 +9,29 @@ export const fetchUser = async (num: number) => {
     }
 };
 
+export const fetchJobs = async () => {
+    const res = await fetch(`http://localhost:8000/api/jobs/getall`, {
+        method: "GET"
+    });
+
+    if (res.ok) {
+        const data = await res.json();
+        return data;
+    }
+};
+
+export const fetchJob = async (num: number) => {
+    const res = await fetch(`http://localhost:8000/api/jobs/${num}`, {
+        method: "GET"
+    });
+
+    if (res.ok) {
+        const data = await res.json();
+        return data;
+    }
+};
+
+
 export const getAllUsers = async () => {
     const res = await fetch(`http://localhost:8000/api/users/getall`, {
         method: "GET"
--- frontend/app/jobs/page.tsx
+++ frontend/app/jobs/page.tsx
@@ -1,19 +1,30 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import NavBar from "../components/nav";
+import { fetchJob } from "../components/dispatch";
+
 
 export default function Jobs() {
+
+  const [job, setJob] = useState<any>(null)
+
+  useEffect(() => {
+    async function fetchData(){
+      const fetchedJob = await fetchJob(2)
+      setJob(fetchedJob)
+    }
+    fetchData()
+  },[])
+
+  console.log(job, 'jobs+++++++++++++++');
   return (
     <div className="container">
       <NavBar />
-      <div>All Jobs</div>
+      <div>
+        <div>
+        </div>
+      </div>
     </div>
   );
-}
-
-async function getJobs() {
-  const res = await fetch("/jobs");
-
-  if (res.ok) {
-    const data = res.json();
-    return data;
-  }
 }
