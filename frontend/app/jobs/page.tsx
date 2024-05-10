193be464ebd1e05ba9da2682484eb8422bdb66e9
Bao <bao4ltyfe@gmail.com>
2024-05-10 12:11:59 -0700

    added modal

--- frontend/app/jobs/page.tsx
+++ frontend/app/jobs/page.tsx
@@ -57,6 +57,21 @@ export default function Profile() {
         <>
           <div>
             <div className="text-red-500 text-3xl m-10">Your Jobs</div>
+            <label className="btn btn-primary" htmlFor="modal-1">Add New Job</label>
+            <input className="modal-state" id="modal-1" type="checkbox" />
+            <div className="modal">
+              <label className="modal-overlay" htmlFor="modal-1"></label>
+              <div className="modal-content flex flex-col gap-5">
+                <label htmlFor="modal-1" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</label>
+                <h2 className="text-xl">Modal title 1</h2>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolorum voluptate ratione dicta. Maxime cupiditate, est commodi consectetur earum iure, optio, obcaecati in nulla saepe maiores nobis iste quasi alias!</span>
+                <div className="flex gap-3">
+                  <button className="btn btn-error btn-block">Add</button>
+
+                  <button className="btn btn-block">Cancel</button>
+                </div>
+              </div>
+            </div>
             <div>
               {sortByDate(jobs).map((job: Job) => (
                 <Link href={`/jobs/${job.id}`} key={job.id}>
